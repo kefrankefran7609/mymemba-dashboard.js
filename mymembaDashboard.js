e46d3758c055cf948495ccbd80b9c918215274a7
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2022-11-02 08:04:59 -0600

    Update mymembaDashboard.js

--- mymembaDashboard.js
+++ mymembaDashboard.js
@@ -280,7 +280,7 @@ document.querySelector('#templateURL').href = choiceParsed.href
 
 /* Save theme choice in local storage */
 
-document.querySelector('#save').addEventListener('click', () => {
+document.querySelector('#save').addEventListener('click', (e) => {
     if(e.path[0].value === 'sanctuary'){
     let choice = {
     name: "Sanctuary",
